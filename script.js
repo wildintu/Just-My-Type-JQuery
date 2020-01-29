800c6babca12f9fb2a96e4e0a348056e4d6547d3
wildintu <hello@wildintu.com>
2020-01-29 13:30:44 -0600

    keyboard is working

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    //Hide uppercase keyboard
+    $('#keyboard-upper-container').hide();
+
+    //Keyboard Toggling: when shift key is held down, hide lowercase keyboard and show upper case one   
+    $('#karen').keydown(function(event) {
+        let x = event.keyCode;
+        if(x == 16) {
+            $('#keyboard-lower-container').hide();
+            $('#keyboard-upper-container').show();
+        };
+    });
+
+    $('#karen').keyup(function(event) {
+        let x = event.keyCode;
+        if(x == 16) {
+            $('#keyboard-lower-container').show();
+            $('#keyboard-upper-container').hide();
+        };
+    });
+
+    //when keys are pressed, they should be highlighted in browser
+    //console.log(x);
+    $('#karen').keypress(function(event) {
+        let key = $('#' + event.which);
+        //console.log("I work")
+        $(key).css("background-color", "yellow");
+        //unhighlight when key no pressed
+        $('#karen').keyup(function(event) {
+            $(key).css("background-color", "")
+        })   
+    });
+    
+    //let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];   
+    displaySentence();
+    function displaySentence() {
+        let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+        for (let i = 0; i < sentences.length; i++) {
+            display = sentences[i];
+            $('#sentence').text(display);
+
+            
+        };
+    };
+
+});
