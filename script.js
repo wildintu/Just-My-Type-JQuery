5a710d4d3e3ebae4238cd4c4409f944760a64bec
wildintu <hello@wildintu.com>
2020-01-29 18:46:42 -0600

    idk

--- script.js
+++ script.js
@@ -34,22 +34,99 @@ $(document).ready(function() {
     //let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];   
     displaySentence();
     function displaySentence() {
-        let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-        for (let i = 0; i < sentences.length; i++) {
-            display = sentences[i];
-            $('#sentence').text(display);
+        let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+                         'Too ato too nOt enot one totA not anot tOO aNot',
+                         'oat itain oat tain nate eate tea anne inant nean',
+                         'itant eate anot eat nato inate eat anot tain eat',
+                         'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+        
+        $.each(sentences, function(index, value) {
+            //console.log(value);
+            $.each(this, function(index,value) {
+                //console.log(value);
+
+            });
+            
+        });
+
+        for (let x = 0; x < sentences.length; x++) {
+            //console.log(sentences[x]);
+            $('#sentence').text(sentences[x]);
+            for (let y = 0; y < sentences[x].length; y++) {
+                console.log(sentences[x][y]);
+            }
+            
+
+        } 
+
+        
+        
+                
+        
+        
+        
+        
+        
+
+        //if ( key === sentences[x][y]) {
+                    let pixelVal = 4;
+                    $('#karen').keypress(function() {
+                    let yb = $('#yellow-block');
+                    $(yb).css('left', pixelVal + 'px');
+                    pixelVal = pixelVal + 4;
+                    
+                     });
+
+                //};
+        
+        
+        // for (let i = 0; i < sentences.length; i++) {
+        //     for (let j = 0; j < sentences[i].length; j++) {
+        //         //console.log(sentences[i][j]);
+        //         display = sentences[i];
+        //         letter = sentences[i][j];
+        //         //console.log(sentences[i][j]);
+        //         $('#sentence').text(display);
+
+
+
+                
+                //let letterPushed = $();
+                //console.log(letterPushed);
+                // if (letter === keyLetter) {
+                //     let pixelVal = 4;
+                //     $('#karen').keypress(function() {
+                //     let yb = $('#yellow-block');
+                //     $(yb).css('left', pixelVal + 'px');
+                //     pixelVal = pixelVal + 4;
+                    
+                     //});
+
+                //};
+
+
+                // let pixelVal = 4;
+                // $('#karen').keypress(function() {
+                // let yb = $('#yellow-block');
+                // $(yb).css('left', pixelVal + 'px');
+                // pixelVal = pixelVal + 4;
+                
+                // });
+            }
+            
+
 
             //get yellow block to move left
-            let pixelVal = 4;
-            $('#karen').keypress(function() {
-                let yb = $('#yellow-block');
-                $(yb).css('left', pixelVal + 'px');
-                pixelVal = pixelVal + 4;
+            // let pixelVal = 4;
+            // $('#karen').keypress(function() {
+            //     let yb = $('#yellow-block');
+            //     $(yb).css('left', pixelVal + 'px');
+            //     pixelVal = pixelVal + 4;
                 
-            })
+            // });
             
             
-        };
-    };
+       //};
+    //};
 
 });
